dd9d45eab565f0da54ca38389cc9c26d0ed0e8b6
albar <mubarid.albar@outlook.com>
2024-12-23 13:17:01 +0700

    add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,27 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug('EdenEast/nightfox.nvim')
+Plug('nvim-tree/nvim-web-devicons')
+Plug('nvim-tree/nvim-tree.lua')
+
+vim.call('plug#end')
+
+vim.cmd('set termguicolors')
+vim.cmd('colorscheme nordfox')
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup()
+
+vim.cmd('set number')
+vim.cmd('set autochdir')
+
+vim.cmd('cnoreabbrev ft NvimTreeOpen')
+
+vim.cmd('set shiftwidth=2 smarttab')
+vim.cmd('set expandtab')
